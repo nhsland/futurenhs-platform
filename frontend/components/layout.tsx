21c3535c017235ed9188e2996e9d7c939ea106c4
Tracy Wu <sytracywu@gmail.com>
2020-07-02 17:34:25 +0100

    Clean up broken bits where it said your name (#48)

--- frontend/components/layout.tsx
+++ frontend/components/layout.tsx
@@ -4,7 +4,6 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "[Your Name]";
 export const siteTitle = "Next.js Sample Website";
 
 export default function Layout({
@@ -34,9 +33,7 @@ export default function Layout({
             <img
               src="/images/profile.jpg"
               className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
-              alt={name}
             />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
@@ -45,15 +42,9 @@ export default function Layout({
                 <img
                   src="/images/profile.jpg"
                   className={`${styles.headerImage} ${utilStyles.borderCircle}`}
-                  alt={name}
                 />
               </a>
             </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
           </>
         )}
       </header>
