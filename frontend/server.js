0686e145b78a08b828ec4a1ca3774e82c5243ab2
David Laban <david.laban@red-badger.com>
2020-07-08 14:22:31 +0100

    proxy /hello to hello-world in dev server

---
+++ frontend/server.js
@@ -0,0 +1,52 @@
+/// This is taken from the next.js reverse proxy example, and should
+/// only be used for local development.
+/// https://github.com/vercel/next.js/tree/master/examples/with-custom-reverse-proxy
+/* eslint-disable no-console */
+const express = require('express')
+const next = require('next')
+
+const devProxy = {
+  '/hello': {
+    target: 'http://hello-world.hello-world/',
+    changeOrigin: true,
+  },
+}
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const env = process.env.NODE_ENV
+const dev = env !== 'production'
+const app = next({
+  dir: '.', // base directory where everything is, could move to src later
+  dev,
+})
+
+const handle = app.getRequestHandler()
+
+let server
+app
+  .prepare()
+  .then(() => {
+    server = express()
+
+    // Set up the proxy.
+    if (dev && devProxy) {
+      const { createProxyMiddleware } = require('http-proxy-middleware')
+      Object.keys(devProxy).forEach(function (context) {
+        server.use(context, createProxyMiddleware(devProxy[context]))
+      })
+    }
+
+    // Default catch-all handler to allow Next.js to handle all other routes
+    server.all('*', (req, res) => handle(req, res))
+
+    server.listen(port, (err) => {
+      if (err) {
+        throw err
+      }
+      console.log(`> Ready on port ${port} [${env}]`)
+    })
+  })
+  .catch((err) => {
+    console.log('An error occurred, unable to start the server')
+    console.log(err)
+  })
