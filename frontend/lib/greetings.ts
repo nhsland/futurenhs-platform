aab089a0c17d0314107c9dd9bf0d16ae9032030e
Tracy Wu <sytracywu@gmail.com>
2020-07-14 14:22:16 +0100

    Client side rendering (#64)
    * client side working
    
    * adding axios back in
    
    * adding isomorphic
    
    * adding in comment on shallow

---
+++ frontend/lib/greetings.ts
@@ -0,0 +1,8 @@
+import "isomorphic-fetch";
+
+export const getGreeting = async (name: string | string[]) => {
+  const res = await fetch(`http://hello-world.hello-world/hello/${name}`);
+
+  const jsonFormatted = await res.text();
+  return jsonFormatted;
+};
--- frontend/pages/greetings/[name].tsx
+++ frontend/pages/greetings/[name].tsx
@@ -1,38 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import fetch from "node-fetch";
+import axios from "axios";
 
 import { GetServerSideProps } from "next";
+import { getGreeting } from "../../lib/greetings";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const res = await fetch(
-    `http://hello-world.hello-world/hello/${context?.params?.name}`
-  );
-
-  console.log("*****8888", typeof window);
-
-  const greeting = await res.text();
+  const nameProp = context?.params?.name || "";
+  const greeting = await getGreeting(nameProp);
 
   return {
     props: {
       greeting,
+      nameProp,
     },
   };
 };
 
-const GreetingForm = ({ greeting }: { greeting: object }) => {
+const GreetingForm = ({
+  greeting,
+  nameProp,
+}: {
+  greeting: object;
+  nameProp: string;
+}) => {
   const [inputValue, setInputValue] = useState("");
+  const [inputValueClient, setInputValueClient] = useState("");
+  const [axiosValue, setAxiosValue] = useState("");
+
+  const callAndSetGreeting = async (name: string) => {
+    try {
+      const res = await axios.get(`/hello/${name}`);
+
+      setAxiosValue(res.data);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    callAndSetGreeting(nameProp);
+  }, []);
+
   return (
     <>
       <section>
+        <div>Server side rendering example</div>
         <div>Enter a name</div>
-
         <div>{greeting}</div>
         <input
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-
+        {/* Shallow routing allows you to change the URL without running data fetching methods again, that includes getServerSideProps, getStaticProps, and getInitialProps.
+        ou'll receive the updated pathname and the query via the router object (added by useRouter or withRouter), without losing state. https://nextjs.org/docs/routing/shallow-routing */}
         <Link href="/greetings/[name]" as={`/greetings/${inputValue}`}>
           <button>greet</button>
         </Link>
@@ -41,6 +62,20 @@ const GreetingForm = ({ greeting }: { greeting: object }) => {
         <Link href="/">
           <a>home</a>
         </Link>
+      </section>
+      <section>
+        <div>Client side rendering example</div>
+        <div>Enter a name</div>
+
+        <div>{axiosValue}</div>
+        <input
+          value={inputValueClient}
+          onChange={(e) => setInputValueClient(e.target.value)}
+        />
+
+        <button onClick={() => callAndSetGreeting(inputValueClient)}>
+          greet
+        </button>
       </section>
     </>
   );
