a2d68455e5aec80308bda6d6e08eaa3e2298b291
Matt Paul <mattpaul001@icloud.com>
2020-07-14 16:42:32 +0100

    Terraform the azure vault

---
+++ infrastructure/environments/setup-dev-shared/main.tf
@@ -0,0 +1,93 @@
+provider "azurerm" {
+  version = "=2.14.0"
+  features {}
+  subscription_id = "4a4be66c-9000-4906-8253-6a73f09f418d"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
+
+provider "tls" {
+  version = "~> 2.1"
+}
+
+terraform {
+  required_version = "0.12.25"
+  backend "azurerm" {
+    container_name       = "vault-container"
+    key                  = "dev-vault.terraform.tfstate"
+    resource_group_name  = "vault"
+    storage_account_name = "fnhstfstatedevshared"
+  }
+}
+
+resource "tls_private_key" "sealed_secret" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "tls_self_signed_cert" "sealed_secret" {
+  key_algorithm         = tls_private_key.sealed_secret.algorithm
+  private_key_pem       = tls_private_key.sealed_secret.private_key_pem
+  validity_period_hours = 87600 // 10 years
+  allowed_uses          = []
+  subject {
+    common_name  = "dev-sealed-secret-cert"
+    organization = "fnhs"
+  }
+}
+
+data "azurerm_client_config" "current" {
+}
+
+resource "azurerm_key_vault" "vault" {
+  name                = "fnhs-shared-dev"
+  location            = "westeurope"
+  resource_group_name = "vault"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+
+  sku_name = "premium"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "create",
+      "get",
+    ]
+
+    secret_permissions = [
+      "set",
+      "get",
+      "delete",
+    ]
+  }
+
+  tags = {
+    environment = "dev-shared"
+  }
+}
+
+
+// This is the k8s secret that is required by devs to use for sealing secrets, using the cert that we created above.
+// NOTE on sealedsecrets.bitnami.com/sealed-secrets-key: active
+// On startup, the controller searches for this secret within its namespace.  The public portion of key pair should be ourput in logs
+resource "azurerm_key_vault_secret" "sealed_secret" {
+  name         = "sealed-secret-yaml"
+  value        = <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: sealed-secret-key
+  namespace: kube-system
+  labels:
+    sealedsecrets.bitnami.com/sealed-secrets-key: active
+type: kubernetes.io/tls
+data:
+  tls.crt: ${base64encode(tls_self_signed_cert.sealed_secret.cert_pem)}
+  tls.key: ${base64encode(tls_private_key.sealed_secret.private_key_pem)}
+EOF
+  key_vault_id = azurerm_key_vault.vault.id
+}
