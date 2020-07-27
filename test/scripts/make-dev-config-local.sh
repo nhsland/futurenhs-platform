c93f9f1ee656599c6d5714d455332f6af6e77042
declanslevin <33623842+declanslevin@users.noreply.github.com>
2020-07-27 16:02:39 +0100

    Add dev cluster local cypress script

---
+++ test/scripts/make-dev-config-local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+NAME="${1:?"Please enter your name as first argument"}"
+
+cat $REPO_ROOT/test/cypress-prod.json \
+    | sed "s/fnhs.westeurope.cloudapp.azure.com/fnhs-dev-$NAME.westeurope.cloudapp.azure.com/" \
+    > $REPO_ROOT/test/cypress.json
+
+cd $REPO_ROOT/test && yarn cypress open
+rm $REPO_ROOT/test/cypress.json
