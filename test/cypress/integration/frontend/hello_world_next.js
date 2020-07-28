371c6d11f1fc3e871d389f37f20d126cf4f0a9ee
declanslevin <33623842+declanslevin@users.noreply.github.com>
2020-07-28 16:08:55 +0100

    cleanup test

--- test/cypress/integration/frontend/hello_world_next.js
+++ test/cypress/integration/frontend/hello_world_next.js
@@ -2,8 +2,6 @@ describe("Hello world", () => {
   it("Visits the home page", () => {
     cy.visit("/");
     var baseUrl = Cypress.config().baseUrl;
-    console.log(baseUrl);
-    // cy.url().should("include", "http://bs-local.com:3000");
     cy.url().should("include", baseUrl);
   });
 
