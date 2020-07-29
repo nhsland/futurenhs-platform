afc9b2dc26e7e80073684a9f4738aac21a237d68
David Laban <david.laban@red-badger.com>
2020-07-29 15:19:36 +0100

    use output variables rather than data sources

--- infrastructure/modules/databases/variables.tf
+++ infrastructure/modules/databases/variables.tf
@@ -1,10 +1,6 @@
 variable "environment" {
   description = "Namespace for all resources. Eg 'production' 'dev-jane'"
 }
-# TODO: I don't think we need this anymore if we have the terraform provider.
-variable "postgresql_admin_password" {
-  description = "Admin password for the postgresql server"
-}
 variable "postgresql_server_name" {
   description = "Admin password for the postgresql server"
 }
