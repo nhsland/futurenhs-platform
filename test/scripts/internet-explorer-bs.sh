c6373d056078fbe6fc93d8c9a80f422d152dbaff
Jan Kuehle <jan.kuehle@red-badger.com>
2020-08-06 14:09:46 +0100

    Change prod domain to beta.future.nhs.uk

--- test/scripts/internet-explorer-bs.sh
+++ test/scripts/internet-explorer-bs.sh
@@ -12,7 +12,7 @@ elif [[ "$ENV" == dev ]]; then
 	NAME="${2:?"If you wish to use a dev cluster, please enter your name as the second argument."}"
 	IE_BASE_URL="https://fnhs-dev-$NAME.westeurope.cloudapp.azure.com/"
 elif [[ "$ENV" == prod ]]; then
-	IE_BASE_URL="https://fnhs.westeurope.cloudapp.azure.com/"
+	IE_BASE_URL="https://beta.future.nhs.uk/"
 fi
 
 cd $REPO_ROOT/test && IE_BASE_URL=$IE_BASE_URL yarn mocha ./internet-explorer/internet_explorer_hello_world.js
