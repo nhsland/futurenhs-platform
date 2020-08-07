ab449a459738dd2e594cf60bad6616f46dcbb816
Jan Kuehle <jan.kuehle@red-badger.com>
2020-08-07 13:37:32 +0100

    Report names of missing env vars in health check
    This should not expose any unwanted information publicly. If there is an
    error, Kubernetes won't make the app public in the first place.

--- frontend/pages/api/health.ts
+++ frontend/pages/api/health.ts
@@ -1,13 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+const requiredEnvVars = ["EVENTGRID_TOPIC_ENDPOINT", "EVENTGRID_TOPIC_KEY"];
+
 export default (_: NextApiRequest, res: NextApiResponse) => {
-  if (
-    !process.env.EVENTGRID_TOPIC_ENDPOINT ||
-    !process.env.EVENTGRID_TOPIC_KEY
-  ) {
+  const missingEnvVars = requiredEnvVars.filter((name) => !process.env[name]);
+  if (missingEnvVars.length > 0) {
     res
       .status(500)
-      .json({ status: "Error", message: "Missing environment variables" });
+      .json({
+        status: "Error",
+        message: `Missing environment variables: ${missingEnvVars.join(", ")}`,
+      });
   } else {
     res.status(200).json({ status: "Healthy" });
   }
