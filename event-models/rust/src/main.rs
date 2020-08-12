87ce5e81b7f0fd20cf9f27f0fda8fa43a7f99c83
Jan Kuehle <jan.kuehle@red-badger.com>
2020-08-12 09:03:53 +0100

    Custom deserialize

--- event-models/rust/src/main.rs
+++ event-models/rust/src/main.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let event = fnhs_event_models::parse()?;
     println!("Event: {:?}", event);
-    fnhs_event_models::custom_parse()?;
+    let event = fnhs_event_models::custom_parse()?;
+    println!("Custom Event: {:?}", event);
     Ok(())
 }
