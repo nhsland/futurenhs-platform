43390e9bc63e06c99a9ce6688827a1655ba33a1c
Jan Kuehle <jan.kuehle@red-badger.com>
2020-08-13 11:29:26 +0100

    Move custom serialization into different file

--- event-models/rust/src/main.rs
+++ event-models/rust/src/main.rs
@@ -1,7 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let event = fnhs_event_models::parse()?;
     println!("Event: {:?}", event);
-    let event = fnhs_event_models::custom_parse()?;
+    fnhs_event_models::custom::test_stringify()?;
+    let event = fnhs_event_models::custom::test_parse()?;
     println!("Custom Event: {:?}", event);
     Ok(())
 }
