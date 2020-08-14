1f4d0b1d056136a8687e7125da4cf033766d4df4
Jan Kuehle <jan.kuehle@red-badger.com>
2020-08-14 09:02:06 +0100

    Custom macro

--- event-models/rust/src/lib.rs
+++ event-models/rust/src/lib.rs
@@ -7,6 +7,10 @@ schemafy::schemafy!(
     root: Event
     "../schema.json"
 );
+pub mod fnhs_models {
+    use serde::{Deserialize, Serialize};
+    fnhs_event_models_generate::event_models!("../schema.json");
+}
 
 pub fn parse() -> Result<Event, Box<dyn std::error::Error>> {
     let event: Event =
