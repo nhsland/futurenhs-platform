9eba9f6ce24367b113cb3bb9a46ca3c16ebeff1f
Jan Kuehle <jan.kuehle@red-badger.com>
2020-09-08 14:15:36 +0100

    Dynamic asset prefix in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 const withImages = require("next-images");
-const isProd = process.env.NODE_ENV === "production";
 
 module.exports = withImages({
   inlineImageLimit: 0,
-  assetPrefix: isProd ? "__FNHS_ASSET_PREFIX__" : "",
+  assetPrefix: process.env.ORIGIN,
 });
