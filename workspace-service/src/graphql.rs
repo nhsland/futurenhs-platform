fe85a1fdab7da6abba1bcce27821430267e650d1
David Laban <david.laban@red-badger.com>
2020-09-08 16:29:55 +0100

    graphql for folders

--- workspace-service/src/graphql.rs
+++ workspace-service/src/graphql.rs
@@ -27,6 +27,29 @@ impl From<db::Workspace> for Workspace {
     }
 }
 
+#[SimpleObject(desc = "A folder")]
+pub struct Folder {
+    #[field(desc = "The id of the folder")]
+    id: ID,
+    #[field(desc = "The title of the folder")]
+    title: String,
+    #[field(desc = "The description of the folder")]
+    long_description: String,
+    #[field(desc = "The workspace that this folder is in")]
+    workspace: ID,
+}
+
+impl From<db::Folder> for Folder {
+    fn from(d: db::Folder) -> Self {
+        Self {
+            id: d.id.into(),
+            title: d.title,
+            long_description: d.long_description,
+            workspace: d.workspace.into(),
+        }
+    }
+}
+
 #[InputObject]
 struct NewWorkspace {
     title: String,
@@ -35,6 +58,19 @@ struct NewWorkspace {
 
 #[InputObject]
 struct UpdateWorkspace {
+    title: String,
+    long_description: String,
+}
+
+#[InputObject]
+struct NewFolder {
+    title: String,
+    long_description: String,
+    workspace: ID,
+}
+
+#[InputObject]
+struct UpdateFolder {
     title: String,
     long_description: String,
 }
@@ -77,6 +113,31 @@ impl Query {
         let workspace = db::Workspace::find_by_id(id, pool).await?;
         Ok(workspace.into())
     }
+
+    #[field(desc = "Get all Folders in a workspace")]
+    async fn folders_by_workspace(
+        &self,
+        context: &Context<'_>,
+        workspace: ID,
+    ) -> FieldResult<Vec<Folder>> {
+        let pool = context.data()?;
+        let workspace = Uuid::parse_str(workspace.as_str())?;
+        let folders = db::Folder::find_by_workspace(workspace, pool).await?;
+        Ok(folders.into_iter().map(Into::into).collect())
+    }
+
+    #[field(desc = "Get folder by ID")]
+    async fn folder(&self, context: &Context<'_>, id: ID) -> FieldResult<Folder> {
+        self.get_folder(context, id).await
+    }
+
+    #[entity]
+    async fn get_folder(&self, context: &Context<'_>, id: ID) -> FieldResult<Folder> {
+        let pool = context.data()?;
+        let id = Uuid::parse_str(id.as_str())?;
+        let folder = db::Folder::find_by_id(id, pool).await?;
+        Ok(folder.into())
+    }
 }
 
 pub struct Mutation;
@@ -95,7 +156,7 @@ impl Mutation {
         Ok(workspace.into())
     }
 
-    #[field(desc = "Update workspace(returns updated workspace")]
+    #[field(desc = "Update workspace (returns updated workspace")]
     async fn update_workspace(
         &self,
         context: &Context<'_>,
@@ -120,6 +181,42 @@ impl Mutation {
         let workspace = db::Workspace::delete(Uuid::parse_str(id.as_str())?, pool).await?;
 
         Ok(workspace.into())
+    }
+
+    #[field(desc = "Create a new folder (returns the created folder)")]
+    async fn create_folder(&self, context: &Context<'_>, folder: NewFolder) -> FieldResult<Folder> {
+        let pool = context.data()?;
+        let workspace = Uuid::parse_str(folder.workspace.as_str())?;
+        let folder =
+            db::Folder::create(folder.title, folder.long_description, workspace, pool).await?;
+        Ok(folder.into())
+    }
+
+    #[field(desc = "Update folder (returns updated folder")]
+    async fn update_folder(
+        &self,
+        context: &Context<'_>,
+        id: ID,
+        folder: UpdateFolder,
+    ) -> FieldResult<Folder> {
+        let pool = context.data()?;
+        let folder = db::Folder::update(
+            Uuid::parse_str(id.as_str())?,
+            folder.title,
+            folder.long_description,
+            pool,
+        )
+        .await?;
+
+        Ok(folder.into())
+    }
+
+    #[field(desc = "Delete folder (returns deleted folder")]
+    async fn delete_folder(&self, context: &Context<'_>, id: ID) -> FieldResult<Folder> {
+        let pool = context.data()?;
+        let folder = db::Folder::delete(Uuid::parse_str(id.as_str())?, pool).await?;
+
+        Ok(folder.into())
     }
 }
 
