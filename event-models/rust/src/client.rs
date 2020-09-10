3b915f1b5cea4d91173b3cc0ecb2bcf856331b0f
Jan Kuehle <jan.kuehle@red-badger.com>
2020-09-10 11:44:21 +0100

    Move Client struct and impl close together

--- event-models/rust/src/client.rs
+++ event-models/rust/src/client.rs
@@ -6,6 +6,33 @@ pub struct Client {
     key: String,
 }
 
+impl Client {
+    pub fn new(topic_endpoint: String, topic_key: String) -> Self {
+        Self {
+            url: format!(
+                "https://{}/api/events?api-version=2018-01-01",
+                topic_endpoint
+            ),
+            key: topic_key,
+        }
+    }
+
+    /// Publish events to Azure EventGrid.
+    ///
+    /// See also: https://docs.microsoft.com/en-us/rest/api/eventgrid/dataplane/publishevents/publishevents
+    pub async fn publish_events(&self, events: &[Event]) -> Result<(), PublishEventsError> {
+        let res = surf::post(&self.url)
+            .set_header("aeg-sas-key", &self.key)
+            .body_json(&events)?
+            .await?;
+        if res.status() == 200 {
+            Ok(())
+        } else {
+            Err(PublishEventsError::Server(res.status().as_u16()))
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum PublishEventsError {
     Internal(String),
@@ -31,33 +58,6 @@ impl std::fmt::Display for PublishEventsError {
         match self {
             Self::Internal(err) => write!(f, "Internal: {}", err),
             Self::Server(status) => write!(f, "Server: {}", status),
-        }
-    }
-}
-
-impl Client {
-    pub fn new(topic_endpoint: String, topic_key: String) -> Self {
-        Self {
-            url: format!(
-                "https://{}/api/events?api-version=2018-01-01",
-                topic_endpoint
-            ),
-            key: topic_key,
-        }
-    }
-
-    /// Publish events to Azure EventGrid.
-    ///
-    /// See also: https://docs.microsoft.com/en-us/rest/api/eventgrid/dataplane/publishevents/publishevents
-    pub async fn publish_events(&self, events: &[Event]) -> Result<(), PublishEventsError> {
-        let res = surf::post(&self.url)
-            .set_header("aeg-sas-key", &self.key)
-            .body_json(&events)?
-            .await?;
-        if res.status() == 200 {
-            Ok(())
-        } else {
-            Err(PublishEventsError::Server(res.status().as_u16()))
         }
     }
 }
