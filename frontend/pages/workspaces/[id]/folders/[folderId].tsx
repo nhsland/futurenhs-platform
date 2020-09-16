971dadd68b631629c491249502c769d0ed2e33fd
Matt Paul <mattpaul001@icloud.com>
2020-09-16 17:11:29 +0100

    Fetch folder for homepage

--- frontend/pages/workspaces/[id]/folders/[folderId].tsx
+++ frontend/pages/workspaces/[id]/folders/[folderId].tsx
@@ -1,5 +1,72 @@
 import React from "react";
 
-const Hello = () => <h1>Welcome to your folder</h1>;
+import { GraphQLClient } from "graphql-request";
+import { GetServerSideProps } from "next";
+import styled from "styled-components";
 
-export default Hello;
+import { Head } from "../../../../components/Head";
+import { Header } from "../../../../components/Header";
+import { MainHeading } from "../../../../components/MainHeading";
+// import { Navigation } from "../../../../components/Navigation";
+import { PageLayout } from "../../../../components/PageLayout";
+import { requireAuthentication } from "../../../../lib/auth";
+import { getSdk, Folder } from "../../../../lib/generated/graphql";
+
+export const getServerSideProps: GetServerSideProps<Props> = requireAuthentication(
+  async (context) => {
+    const client = new GraphQLClient(
+      "http://workspace-service.workspace-service/graphql"
+    );
+    const sdk = getSdk(client);
+    const folderId = (context.params?.folderId as string) || "";
+
+    const { folder } = await sdk.GetFolderById({ id: folderId });
+    // const { foldersByWorkspace } = await sdk.FoldersByWorkspace({
+    //   workspace: workspaceId,
+    // });
+
+    return {
+      props: {
+        // folders: foldersByWorkspace,
+        folder,
+      },
+    };
+  }
+);
+
+const PageContent = styled.section`
+  flex-grow: 3;
+  min-height: 100vh;
+  padding-top: 24px;
+  padding-left: 10%;
+  padding-right: 10%;
+  ${({ theme }) => `
+  background-color: ${theme.colorNhsukWhite};
+  `}
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+`;
+
+interface Props {
+  folder: Folder;
+  // workspace: Workspace;
+}
+const FolderHomepage = ({ folder }: Props) => (
+  <>
+    <Head title={folder.title} />
+    <PageLayout>
+      <Header />
+      <ContentWrapper>
+        {/* <Navigation workspace={workspace} folders={workspace.folders} /> */}
+        <PageContent>
+          <MainHeading>{folder.title}</MainHeading>
+          <p>{folder.description}</p>
+        </PageContent>
+      </ContentWrapper>
+    </PageLayout>
+  </>
+);
+
+export default FolderHomepage;
