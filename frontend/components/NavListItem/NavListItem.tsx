ea8121fe98a37812d1ff994a3a5510a832635b59
David Laban <david.laban@red-badger.com>
2020-09-17 15:16:58 +0100

    img alignment, border radius, no underline

--- frontend/components/NavListItem/NavListItem.tsx
+++ frontend/components/NavListItem/NavListItem.tsx
@@ -15,6 +15,7 @@ interface Props {
 
 const ListItem = styled.li<any>`
   list-style-type: none;
+  border-radius: 4px;
   &:hover {
     ${({ theme }) => `
     background-color: ${theme.colorNhsukGrey4}
@@ -23,9 +24,16 @@ const ListItem = styled.li<any>`
   a {
     display: flex;
     padding-left: 8px;
+    text-decoration: none;
     &:focus {
       box-shadow: none;
     }
+  }
+  img {
+    align-self: baseline;
+    padding-top: 2px;
+    /* stop things jumping about while svgs load */
+    width: 24px;
   }
   div {
     ${({ theme }) => `
@@ -40,7 +48,7 @@ const icons: { [key: string]: string } = {
 };
 
 const NavListItem = ({ active, item, workspaceId }: Props) => (
-  <ListItem style={{ background: active ? "#ffeb3b" : "" }}>
+  <ListItem style={active ? { background: "#ffeb3b", fontWeight: 700 } : {}}>
     <Link href={`/workspaces/${workspaceId}/folders/${item.id}`}>
       <a>
         <img src={active ? icons["open"] : icons["closed"]} />
