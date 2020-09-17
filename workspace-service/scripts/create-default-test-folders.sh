000644e18395b45950dcd2dabc5f70f8e32dbe4e
Matt Paul <mattpaul001@icloud.com>
2020-09-17 20:59:49 +0100

    Shfmt

--- workspace-service/scripts/create-default-test-folders.sh
+++ workspace-service/scripts/create-default-test-folders.sh
@@ -15,16 +15,16 @@ WORKSPACE_TITLE="Demo workspace"
 
 CURRENT_CONTEXT=$(kubectl config current-context)
 if [ "$ENVIRONMENT" != "$CURRENT_CONTEXT" ]; then
-  echo "You want to populate:    $ENVIRONMENT"
-  echo "Your current content is: $CURRENT_CONTEXT"
-  echo "Please change your current context using:"
-  echo "    kubectl config use-context $ENVIRONMENT"
-  echo "or"
-  echo "    az account set --subscription \$SUBSCRIPTION_ID && az aks get-credentials --resource-group=platform-$ENVIRONMENT --name=$ENVIRONMENT"
-  echo "Once that is done, please run:"
-  echo "    kubefwd services -n workspace-service"
-  echo "in another tab and try again."
-  exit 1
+	echo "You want to populate:    $ENVIRONMENT"
+	echo "Your current content is: $CURRENT_CONTEXT"
+	echo "Please change your current context using:"
+	echo "    kubectl config use-context $ENVIRONMENT"
+	echo "or"
+	echo "    az account set --subscription \$SUBSCRIPTION_ID && az aks get-credentials --resource-group=platform-$ENVIRONMENT --name=$ENVIRONMENT"
+	echo "Once that is done, please run:"
+	echo "    kubefwd services -n workspace-service"
+	echo "in another tab and try again."
+	exit 1
 fi
 
 ./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "FutureNHS Case Study Library"
