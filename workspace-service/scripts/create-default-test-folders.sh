3362807d56bfe694b333c4f56689bf82fafb4f01
David Laban <david.laban@red-badger.com>
2020-09-17 17:09:44 +0100

    more workspace names

--- workspace-service/scripts/create-default-test-folders.sh
+++ workspace-service/scripts/create-default-test-folders.sh
@@ -6,7 +6,7 @@ cd $(dirname $0)
 
 USAGE="
 
-USAGE: $(basename $0) dev-\$FNHSNAME WORKSPACE_TITLE
+USAGE: $(basename $0) dev-\$FNHSNAME
 
 "
 
@@ -27,6 +27,24 @@ if [ "$ENVIRONMENT" != "$CURRENT_CONTEXT" ]; then
   exit 1
 fi
 
-./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Mental Capacity Act"
-./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Resources"
-./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Safeguarding Annual Report"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "FutureNHS Case Study Library"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Getting started"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Platform FAQs"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Support"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Get involved"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Enhance your workspace"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Member stories"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Coronavirus (COVID-19)"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Communications resources"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Administration"
+
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Strategic Partners"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Uploads"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Workspace Navigation"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Data"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Information and Analysis"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Evidence"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Community, Networks and Learning"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Workspace Insights"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "Good News"
+./create-folder-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE" "NHS COVID-19 Data Store"
