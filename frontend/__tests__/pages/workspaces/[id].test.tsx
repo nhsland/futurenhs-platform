186493bb855300db133bd61a06339842342b9a36
David Laban <david.laban@red-badger.com>
2020-09-21 16:36:24 +0100

    workspace homepage snapshot test

---
+++ frontend/__tests__/pages/workspaces/[id].test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { render } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+
+import theme from "../../../lib/fixtures/theme.json";
+import WorkspaceHomepage from "../../../pages/workspaces/[id]";
+
+describe(WorkspaceHomepage, () => {
+  test("renders with folders matching snapshot", () => {
+    const workspace = { title: "hospital", id: "1" };
+    const folders = [{ id: "f1", title: "folder 1" }];
+
+    const container = render(
+      <ThemeProvider theme={theme}>
+        <WorkspaceHomepage workspace={workspace} folders={folders} />
+      </ThemeProvider>
+    );
+    expect(container.asFragment()).toMatchSnapshot();
+  });
+});
