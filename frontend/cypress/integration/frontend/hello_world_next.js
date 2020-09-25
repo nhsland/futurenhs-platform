2753a013c45d296c205c387880bc18197be2860b
edcompton <edmund.compton@red-badger.com>
2020-09-25 16:52:25 +0300

    Add cypress to frontend CI branch.
    - Move basic cypress tests to frontend.
    
    - Configure github Actions to pick up a new cypress action.

---
+++ frontend/cypress/integration/frontend/hello_world_next.js
@@ -0,0 +1,11 @@
+describe("Hello world", () => {
+  it("Visits the home page", () => {
+    cy.visit("/");
+    const baseUrl = Cypress.config().baseUrl;
+    cy.url().should("include", baseUrl);
+  });
+
+  it("Renders page elements", () => {
+    cy.get("p").should("contain", "The new Future is coming...");
+  });
+});
