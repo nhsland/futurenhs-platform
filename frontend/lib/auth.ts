4896e5853383f35f24eae9f90e8eb851bc222f6f
Stuart Harris <stuart.harris@red-badger.com>
2020-09-28 09:21:29 +0100

    reinstate auth.ts and redirect for private.tsx

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -1,5 +1,28 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+
+import { redirect } from "./redirect";
+
 export interface User {
   id: string;
   name: string;
   emails: string[];
 }
+
+export const requireAuthentication = <P>(
+  getServerSideProps: (
+    context: GetServerSidePropsContext,
+    user: User
+  ) => Promise<GetServerSidePropsResult<P>>
+): GetServerSideProps<P> => async (context) => {
+  // @ts-ignore
+  const user: User = context.req.user;
+  if (!user) {
+    return redirect(context, `/auth/login?next=${context.req.url}`);
+  }
+
+  return getServerSideProps(context, user);
+};
