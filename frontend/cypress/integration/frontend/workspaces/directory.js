98d953de05d266eb2cf9ab3a792ed9ff947937f6
Matt Paul <mattpaul001@icloud.com>
2020-09-28 12:34:28 +0100

    Move cypress bits to frontend directory

--- frontend/cypress/integration/frontend/workspaces/directory.js
+++ frontend/cypress/integration/frontend/workspaces/directory.js
@@ -1,13 +1,4 @@
 describe("Workspace Directory", () => {
-  before(() => {
-    cy.login(
-      Cypress.env("TEST_LOGIN_EMAIL_ADDRESS"),
-      Cypress.env("TEST_LOGIN_PASSWORD")
-    );
-    cy.server({ force404: true });
-    cy.route("POST", "https://dc.services.visualstudio.com/v2/track", {});
-  });
-
   // Currently only works against Prod or if you manually create a Workspace named "Selenium Testing" in your local/dev cluster environment
   it("Shows title and loads directory items", () => {
     cy.visit(`/workspaces/directory`);
