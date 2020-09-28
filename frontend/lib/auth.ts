a6f44241c5a11b630505fbf62273da926d94b780
Matt Paul <mattpaul001@icloud.com>
2020-09-28 13:53:16 +0100

    Log in without auth in dev mode

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -20,7 +20,16 @@ export const requireAuthentication = <P>(
 ): GetServerSideProps<P> => async (context) => {
   // @ts-ignore
   const user: User = context.req.user;
-  if (!user) {
+  const dev = process.env.NODE_ENV !== "production";
+
+  if (dev) {
+    const user = {
+      id: "1234",
+      name: "Test user",
+      emails: ["test@example.com"],
+    };
+    return getServerSideProps(context, user);
+  } else if (!user) {
     return redirect(context, `/auth/login?next=${context.req.url}`);
   }
 
