f90bf60cde45cb87ecdb51c42486e81a45521528
Stuart Harris <stuart.harris@red-badger.com>
2020-09-30 13:16:15 +0100

    use graphcache

--- frontend/lib/withUrqlClient.ts
+++ frontend/lib/withUrqlClient.ts
@@ -1,8 +1,11 @@
+import { cacheExchange } from "@urql/exchange-graphcache";
 import { NextPage } from "next";
 import { withUrqlClient as withUrqlClientImpl } from "next-urql";
 import NextApp from "next/app";
+import { dedupExchange, fetchExchange } from "urql";
 
 import { User } from "./auth";
+import { FoldersByWorkspaceDocument } from "./generated/graphql";
 
 const isServerSide = typeof window === "undefined";
 
@@ -10,7 +13,7 @@ export default function withUrqlClient(
   component: NextPage<any> | typeof NextApp
 ) {
   return withUrqlClientImpl(
-    (_ssrExchange, ctx) => {
+    (ssrExchange, ctx) => {
       if (ctx && ctx.req && ctx.res) {
         // @ts-ignore
         const user: User = ctx.req.user;
@@ -22,6 +25,34 @@ export default function withUrqlClient(
         }
       }
       return {
+        exchanges: [
+          dedupExchange,
+          cacheExchange({
+            keys: {},
+            updates: {
+              Mutation: {
+                createFolder: (result, _args, cache) => {
+                  cache.updateQuery(
+                    {
+                      query: FoldersByWorkspaceDocument,
+                      variables: {
+                        // @ts-ignore
+                        workspace: result.createFolder.workspace,
+                      },
+                    },
+                    (data) => {
+                      // @ts-ignore
+                      data?.foldersByWorkspace.push(result.createFolder);
+                      return data;
+                    }
+                  );
+                },
+              },
+            },
+          }),
+          ssrExchange,
+          fetchExchange,
+        ],
         url: isServerSide
           ? "http://workspace-service.workspace-service/graphql"
           : "/api/graphql",
