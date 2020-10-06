d5ac01ebf37021476b6f6ae5c30f7e8db070fa16
Matt Paul <mattpaul001@icloud.com>
2020-10-06 17:04:32 +0100

    Update breakpoints for nav

--- frontend/components/Navigation/Navigation.tsx
+++ frontend/components/Navigation/Navigation.tsx
@@ -17,8 +17,11 @@ const Nav = styled.nav`
   box-sizing: border-box;
   width: 270px;
   ${({ theme }) => `
-  @media (min-width: ${theme.mqBreakpoints.largeDesktop}) {
+    @media (min-width: ${theme.mqBreakpoints.tablet}) {
       display: block;
+      width: 300px;
+    }
+    @media (min-width: ${theme.mqBreakpoints.largeDesktop}) {
       width: 360px;
     }
   `}
