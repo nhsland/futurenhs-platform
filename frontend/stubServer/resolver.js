ea6005477260209b63ed8b31fc0268e608581eaf
Jan Kuehle <jan.kuehle@red-badger.com>
2020-10-06 11:50:37 +0100

    Fix stubServer

--- frontend/stubServer/resolver.js
+++ frontend/stubServer/resolver.js
@@ -1,6 +1,3 @@
-const fs = require("fs");
-const path = require("path");
-
 const createFolderResponse = require("../cypress/fixtures/create-folder-graphql-response.json");
 const folderByWorkspaceResponse = require("../cypress/fixtures/folder-by-workspace-graphql-response.json");
 const folderResponse = require("../cypress/fixtures/folder-graphql-response.json");
@@ -21,18 +18,15 @@ const folderMutation = {
   createFolder: async () => createFolderResponse.data.createFolder,
 };
 
-const schema = fs.readFileSync(
-  path.join(__dirname, "schema", "../../schema.graphql"),
-  "utf8"
-);
+module.exports = (schema) => {
+  const federationResolver = {
+    _service: async () => ({
+      sdl: schema,
+    }),
+  };
 
-const federationResolver = {
-  _service: async () => ({
-    sdl: schema,
-  }),
-};
-
-module.exports = {
-  Query: { ...workspacesResolver, ...folderResolver, ...federationResolver },
-  Mutation: { ...folderMutation },
+  return {
+    Query: { ...workspacesResolver, ...folderResolver, ...federationResolver },
+    Mutation: { ...folderMutation },
+  };
 };
