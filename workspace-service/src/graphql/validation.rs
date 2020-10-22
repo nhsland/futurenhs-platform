67e393a05de708689f76137ded12f4c5eb0c711b
Stuart Harris <stuart.harris@red-badger.com>
2020-10-22 09:57:45 +0100

    refactor as per Jan's suggestion

--- workspace-service/src/graphql/validation.rs
+++ workspace-service/src/graphql/validation.rs
@@ -7,30 +7,28 @@ pub struct ValidationError(ValidationErrors);
 
 impl Display for ValidationError {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        let errors = self.0.clone().into_errors();
-        let errors = errors.iter().map(|(_, v)| v);
-        write!(
-            f,
-            "{}",
-            errors
-                .map(|e| match e {
-                    ValidationErrorsKind::Field(e) => e
-                        .iter()
-                        .map(|e| {
-                            e.message
-                                .clone()
-                                .map(|e| e.into_owned())
-                                .unwrap_or_else(|| "".to_string())
-                        })
-                        .collect::<Vec<String>>()
-                        .join(SEPARATOR),
-                    ValidationErrorsKind::Struct(e) =>
-                        format!("{}", ValidationError::from(*e.clone())),
-                    _ => "".to_string(),
-                })
-                .collect::<Vec<String>>()
-                .join(SEPARATOR)
-        )
+        let s = self
+            .0
+            .errors()
+            .values()
+            .map(|e| match e {
+                ValidationErrorsKind::Field(e) => e
+                    .iter()
+                    .map(|e| {
+                        e.message
+                            .clone()
+                            .map(|e| e.into_owned())
+                            .unwrap_or_else(|| "".to_string())
+                    })
+                    .collect::<Vec<String>>()
+                    .join(SEPARATOR),
+                ValidationErrorsKind::Struct(e) => format!("{}", ValidationError::from(*e.clone())),
+                _ => "".to_string(),
+            })
+            .collect::<Vec<String>>()
+            .join(SEPARATOR);
+
+        write!(f, "{}", s)
     }
 }
 
