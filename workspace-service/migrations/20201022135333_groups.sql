d1a31c99a661b921dc86b42cfc54b22f81ba69c9
Tracy Wu <sytracywu@gmail.com>
2020-10-23 12:11:09 +0100

    updating migration

--- workspace-service/migrations/20201022135333_groups.sql
+++ workspace-service/migrations/20201022135333_groups.sql
@@ -4,15 +4,12 @@ CREATE TABLE IF NOT EXISTS groups (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS group_workspace_grants (
-  is_admin BOOLEAN NOT NULL,
-  group_id uuid NOT NULL REFERENCES groups,
-  workspace_id uuid NOT NULL REFERENCES workspaces,
-  CONSTRAINT group_workspace_grant PRIMARY KEY (is_admin, group_id, workspace_id)
-);
-
 CREATE TABLE IF NOT EXISTS user_groups (
   user_id uuid NOT NULL REFERENCES users,
   group_id uuid NOT NULL REFERENCES groups,
   CONSTRAINT user_group PRIMARY KEY (user_id, group_id)
 );
+
+ALTER TABLE workspaces ADD COLUMN members uuid REFERENCES groups;
+
+ALTER TABLE workspaces ADD COLUMN admins uuid REFERENCES groups;
