4e5fb7e505886bf534b912bd70c738905c2a5f8b
Stuart Harris <stuart.harris@red-badger.com>
2020-10-27 17:15:00 +0000

    fix tests

--- frontend/components/FileTable/FileTable.test.tsx
+++ frontend/components/FileTable/FileTable.test.tsx
@@ -1,30 +1,39 @@
 import React from "react";
 
 import { FileTable } from ".";
+import { File } from "../../lib/generated/graphql";
 import { render } from "../../lib/test-helpers/render";
 
-const fakeFile = {
+const fakeFile: File = {
   id: "fake-file",
-  title: "My Fake File",
-  folder: "fake-folder",
+  createdAt: "2020-10-08T07:10:09Z",
+  description: "test",
+  fileName: "filename.txt",
   fileType: "application/pdf",
+  folder: "fake-folder",
+  latestVersion: "v2",
   modifiedAt: "2020-10-08T07:10:09Z",
+  title: "My Fake File",
 };
 
 describe(FileTable, () => {
-  it("renders title with a surrounding <a> if titleLink=true", () => {
+  it("renders title with a surrounding <a> when given a correct function component", () => {
     const { getByText } = render(
-      <FileTable files={[fakeFile]} workspaceId="face-workspace" titleLink />
+      <FileTable
+        // eslint-disable-next-line react/display-name
+        columns={[{ content: (f: File) => <a href={f.fileName}>{f.title}</a> }]}
+        data={[fakeFile]}
+      />
     );
     expect(getByText("My Fake File").closest("a")).not.toBeNull();
   });
 
-  it("renders title without a surrounding <a> if titleLink=false", () => {
+  it("renders title without a surrounding <a> when the supplied function component has no link", () => {
     const { getByText } = render(
       <FileTable
-        files={[fakeFile]}
-        workspaceId="face-workspace"
-        titleLink={false}
+        // eslint-disable-next-line react/display-name
+        columns={[{ content: (f: File) => <span>{f.title}</span> }]}
+        data={[fakeFile]}
       />
     );
     expect(getByText("My Fake File").closest("a")).toBeNull();
@@ -33,9 +42,9 @@ describe(FileTable, () => {
   it("displays the table heading if given", () => {
     const { getByText } = render(
       <FileTable
-        files={[fakeFile]}
-        workspaceId="face-workspace"
-        titleLink={false}
+        data={[fakeFile]}
+        // eslint-disable-next-line react/display-name
+        columns={[]}
         tableHeading="I am a table"
       />
     );
