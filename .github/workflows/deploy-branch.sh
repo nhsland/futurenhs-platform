bd9bf723532d7b2d43144f4aa48510a65fb9461c
Jan Kuehle <jan.kuehle@red-badger.com>
2020-11-04 17:46:57 +0000

    Disable certain actions if secrets are not available
    Fork PRs don't have access to secrets. The most reliable way to check for this
    seems to be checking the secret values.
    
    Here we just skip steps, that require secrets. Those steps are pushing Docker
    images to our Azure registry and pushing updated manifests to the depployments
    repo. These checks make it easy to test the changes on a dev environment, but
    they aren't otherwise necessary. So it should be okay to skip them.

---
+++ .github/workflows/deploy-branch.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -eux
+
+COMPONENT=${1:?"Require components as first argument, e.g. frontend or workspace-service"}
+
+# Checkout correct branch in the deployments repo
+cd $GITHUB_WORKSPACE/futurenhs-deployments
+git config --local user.email "futurenhs-devs@red-badger.com"
+git config --local user.name "FutureNHS CI/CD"
+if git fetch --unshallow origin pr-$GITHUB_HEAD_REF master; then
+	git checkout origin/pr-$GITHUB_HEAD_REF -b pr-$GITHUB_HEAD_REF
+	git merge origin/master --strategy=recursive --strategy-option=ours
+else
+	git checkout HEAD -b pr-$GITHUB_HEAD_REF
+fi
+
+# Install Kustomize
+cd $HOME
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+
+# Prepare manifests and copy to deployments repo
+cd $GITHUB_WORKSPACE/futurenhs-platform/$COMPONENT/manifests/dev-template
+if [ "$COMPONENT" = "infrastructure" ]; then
+	mkdir -p $GITHUB_WORKSPACE/futurenhs-deployments/cert-manager
+	cp -r $GITHUB_WORKSPACE/futurenhs-platform/infrastructure/kubernetes/cert-manager/* $GITHUB_WORKSPACE/futurenhs-deployments/cert-manager
+	mkdir -p $GITHUB_WORKSPACE/futurenhs-deployments/ingress
+	cp -r $GITHUB_WORKSPACE/futurenhs-platform/infrastructure/kubernetes/ingress/* $GITHUB_WORKSPACE/futurenhs-deployments/ingress
+else
+	$HOME/kustomize edit set image $DIGEST
+	$GITHUB_WORKSPACE/futurenhs-platform/infrastructure/scripts/create-dev-overlays.py
+	mkdir -p $GITHUB_WORKSPACE/futurenhs-deployments/$COMPONENT
+	cp -r $GITHUB_WORKSPACE/futurenhs-platform/$COMPONENT/manifests/* $GITHUB_WORKSPACE/futurenhs-deployments/$COMPONENT
+fi
+
+# Get commit message
+cd $GITHUB_WORKSPACE/futurenhs-platform
+git fetch origin HEAD --deepen=2
+COMMIT_MESSAGE=$(git log --format=%B -n 1 $GITHUB_SHA)
+
+# Commit and push changes to deployments repo
+cd $GITHUB_WORKSPACE/futurenhs-deployments
+
+git add -A
+git diff-index --quiet HEAD || git commit -am "$COMPONENT-branch: $COMMIT_MESSAGE ${TAG:-""}"
+
+declare -i n
+n=0
+until [ $n -ge 5 ]; do
+	git push origin "HEAD:pr-$GITHUB_HEAD_REF" && break
+	n+=1
+	git fetch origin pr-$GITHUB_HEAD_REF
+	git merge origin/pr-$GITHUB_HEAD_REF --strategy=recursive --strategy-option=ours
+done
+
+if [ $n -ge 5 ]; then
+	echo "ran out of retries"
+	exit 1
+fi
