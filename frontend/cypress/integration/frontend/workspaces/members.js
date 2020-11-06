d8ca7bfec52eb7b2d69b8aee0b08bd040175323d
Tracy Wu <sytracywu@gmail.com>
2020-11-06 16:22:52 +0000

    Members cypress tests (#472)
    * stub data and initial tests working
    
    * mobile tests added

--- frontend/cypress/integration/frontend/workspaces/members.js
+++ frontend/cypress/integration/frontend/workspaces/members.js
@@ -7,4 +7,51 @@ describe("Workspace Members page", () => {
     cy.contains("h1", "Workspace members");
     cy.contains("p", "This is a list of all workspace members.");
   });
+
+  context("Desktop - 990px+", () => {
+    beforeEach(() => {
+      cy.viewport(1200, 1080);
+    });
+
+    it("Shows members on desktop", () => {
+      cy.visit(`workspaces/${Cypress.env("TEST_WORKSPACE_ID")}/members`);
+      cy.get("tr")
+        .eq(1)
+        .within(() => {
+          cy.get("td").eq(0).contains("Lisa Pink");
+          cy.get("td").eq(1).contains("a", "lisa.pink@example.com");
+          cy.get("td").eq(2).get(`svg[width="24"]`).click();
+        });
+      cy.get("tr")
+        .eq(2)
+        .within(() => {
+          cy.get("td").eq(0).contains("Permissions");
+        });
+    });
+  });
+
+  context("Mobile - 320px", () => {
+    beforeEach(() => {
+      cy.viewport(320, 720);
+    });
+
+    it("Shows members on mobile", () => {
+      cy.visit(`workspaces/${Cypress.env("TEST_WORKSPACE_ID")}/members`);
+      cy.get("h3")
+        .next("ul")
+        .within(() => {
+          cy.get("li")
+            .eq(0)
+            .within(() => {
+              cy.contains("h4", "Name of user");
+              cy.contains("div", "Lisa Pink");
+              cy.contains("h4", "Email");
+              cy.contains("a", "lisa.pink@example.com");
+              cy.get(`svg[width="24"]`).click();
+              cy.contains("h4", "Permissions");
+              cy.contains("div", "Administrator");
+            });
+        });
+    });
+  });
 });
