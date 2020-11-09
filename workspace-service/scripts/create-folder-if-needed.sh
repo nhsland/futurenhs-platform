3bf2dfd6dc2a03266017b846b54b1bc2239ceb3a
Stuart Harris <stuart.harris@red-badger.com>
2020-11-09 17:37:24 +0000

    pass auth-id and minor shellcheck lints

--- workspace-service/scripts/create-folder-if-needed.sh
+++ workspace-service/scripts/create-folder-if-needed.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
-cd $(dirname $0)
+cd "$(dirname "$0")"
 
 USAGE="
 
-USAGE: $(basename $0) (dev-\$FNHSNAME|local) WORKSPACE_TITLE FOLDER_TITLE [FOLDER_DESCRIPTION]
+USAGE: $(basename "$0") (dev-\$FNHSNAME|local) WORKSPACE_TITLE FOLDER_TITLE [FOLDER_DESCRIPTION]
 
 "
 
@@ -17,19 +17,19 @@ FOLDER_DESCRIPTION=${4:-"Test folder with title $FOLDER_TITLE"}
 
 . ./_context.sh
 
-WORKSPACE_SERVICE_GRAPHQL_ENDPOINT="$(_verify_environment_and_get_graphql_endpoint $ENVIRONMENT)"
+WORKSPACE_SERVICE_GRAPHQL_ENDPOINT="$(_verify_environment_and_get_graphql_endpoint "$ENVIRONMENT")"
 
 workspace=$(./create-workspace-if-needed.sh "$ENVIRONMENT" "$WORKSPACE_TITLE")
-if [ $workspace = "null" ]; then
-	echo "Something went wrong finding/creating your workspace"
-	exit 1
+if [ "$workspace" = "null" ]; then
+  echo "Something went wrong finding/creating your workspace"
+  exit 1
 fi
 
 body=$(
-	jq \
-		--null-input \
-		--arg workspace "$workspace" \
-		'{
+  jq \
+    --null-input \
+    --arg workspace "$workspace" \
+    '{
 		"query": "query FoldersByWorkspace($workspace: ID!) { foldersByWorkspace(workspace: $workspace) { title, id } }",
 		"variables": {
 			"workspace": $workspace,
@@ -37,34 +37,35 @@ body=$(
 	}'
 )
 existing_folders=$(
-	curl \
-		--silent \
-		--show-error \
-		-XPOST \
-		$WORKSPACE_SERVICE_GRAPHQL_ENDPOINT \
-		-H 'Content-Type: application/json' \
-		-d "$body"
+  curl \
+    --silent \
+    --show-error \
+    -XPOST \
+    "$WORKSPACE_SERVICE_GRAPHQL_ENDPOINT" \
+    -H 'x-user-auth-id: feedface-0000-0000-0000-000000000000' \
+    -H 'Content-Type: application/json' \
+    -d "$body"
 )
 
 found=$(
-	echo "$existing_folders" |
-		jq \
-			-r \
-			--arg title "$FOLDER_TITLE" \
-			'.data.foldersByWorkspace | map(select(.title == $title))[0].id'
+  echo "$existing_folders" |
+    jq \
+      -r \
+      --arg title "$FOLDER_TITLE" \
+      '.data.foldersByWorkspace | map(select(.title == $title))[0].id'
 )
 if [ "$found" != "null" ]; then
-	echo $found
-	exit 0
+  echo "$found"
+  exit 0
 fi
 
 body=$(
-	jq \
-		--null-input \
-		--arg workspace "$workspace" \
-		--arg title "$FOLDER_TITLE" \
-		--arg description "$FOLDER_DESCRIPTION" \
-		'{
+  jq \
+    --null-input \
+    --arg workspace "$workspace" \
+    --arg title "$FOLDER_TITLE" \
+    --arg description "$FOLDER_DESCRIPTION" \
+    '{
 			"query": "mutation CreateFolder($workspace: ID!, $title: String!, $description: String!) { createFolder(newFolder: { workspace: $workspace, title: $title,  description: $description }) { id } }",
 			"variables": {
 				"workspace": $workspace,
@@ -74,18 +75,19 @@ body=$(
 		}'
 )
 response=$(
-	curl \
-		--silent \
-		--show-error \
-		-XPOST \
-		$WORKSPACE_SERVICE_GRAPHQL_ENDPOINT \
-		-H 'Content-Type: application/json' \
-		-d "$body"
+  curl \
+    --silent \
+    --show-error \
+    -XPOST \
+    "$WORKSPACE_SERVICE_GRAPHQL_ENDPOINT" \
+    -H 'x-user-auth-id: feedface-0000-0000-0000-000000000000' \
+    -H 'Content-Type: application/json' \
+    -d "$body"
 )
 id=$(echo "$response" | jq -r '.data.createFolder.id')
-if [ $id = "null" ]; then
-	echo "something went wrong! $response"
-	exit 1
+if [ "$id" = "null" ]; then
+  echo "something went wrong! $response"
+  exit 1
 fi
 
-echo $id
+echo "$id"
