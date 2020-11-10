90112ecd4f281ec4e1c2106b4e16f093b8f1425c
declanslevin <33623842+declanslevin@users.noreply.github.com>
2020-11-10 12:12:58 +0000

    Add new div for forgotten password

--- frontend/components/Login/Login.tsx
+++ frontend/components/Login/Login.tsx
@@ -4,11 +4,10 @@ import styled from "styled-components";
 
 const StyledDiv = styled.div`
   ${({ theme }) => `
-    min-height: 421px;
     min-width: 287px;
     background-color: ${theme.colorNhsukGrey5};
-    border-radius: 4px;
-    margin: 40px 0;
+    border-radius: 4px 4px 0px 0px;
+    margin-top: 40px;
 
     @media (min-width: ${theme.mqBreakpoints.tablet}) {
       min-width: 435px;
@@ -20,7 +19,6 @@ const StyledDiv = styled.div`
 
     @media (min-width: ${theme.mqBreakpoints.largeDesktop}) {
       min-width: 481px;
-      min-height: 504px;
     }
 
     @media (min-width: 1200px) {
@@ -38,15 +36,12 @@ const StyledDiv = styled.div`
       padding: 32px 40px;
     }
 
-    label {
-      margin-bottom: 8px;
-    }
-
     label:after {
       content: " *";
     }
 
     input {
+      margin-top: 8px;
       margin-bottom: 24px;
       padding: 8px 12px;
       border: solid 2px #3d4448;
@@ -56,25 +51,12 @@ const StyledDiv = styled.div`
       display: flex;
       flex-direction: column;
       position: relative;
-
-      &:nth-child(2) {
-        input {
-          position: relative;
-          top: -25px;
-          left: 0;
-        }
-      }
     }
 
-    .password-label {
-      display: flex;
-      flex-direction: column;
-
-      a {
-        position: relative;
-        top: 50px;
-      }
+    #forgotPassword {
+      display: none;
     }
+
 
     button {
       width: 97px;
@@ -91,56 +73,79 @@ const StyledDiv = styled.div`
   `}
 `;
 
+const PasswordDiv = styled.div`
+  min-width: 287px;
+  background-color: ${({ theme }) => theme.colorNhsukGrey5};
+  border-radius: 0px 0px 4px 4px;
+  margin-bottom: 40px;
+  padding: 0px 40px 32px 40px;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const Login = () => {
   return (
-    <StyledDiv id="api">
-      {/* <form
-        id="localAccountForm"
-        action="JavaScript:void(0);"
-        className="localAccount"
-        aria-label="Sign in with your email address"
-      >
-        <div className="intro">
-          <h2>Sign in with your email address</h2>
-        </div>
-        <div className="entry">
-          <div className="entry-item">
-            <label htmlFor="email">Email Address</label>
-            <input
-              type="text"
-              id="email"
-              name="Email Address"
-              placeholder="Email Address"
-              value=""
-            />
+    <LoginContainer>
+      <StyledDiv id="api">
+        <form
+          id="localAccountForm"
+          action="JavaScript:void(0);"
+          className="localAccount"
+          aria-label="Sign in with your email address"
+        >
+          <div className="intro">
+            <h2>Sign in with your email address</h2>
           </div>
-          <div className="entry-item">
-            <div className="password-label">
-              <label htmlFor="password">Password</label>
-              <a
-                id="forgotPassword"
-                href="/futurenhsplatform.onmicrosoft.com/B2C_1_logintest/api/CombinedSigninAndSignup/forgotPassword?csrf_token=Q0tQNm51aUlXcXAzamVxNTFuL3c4aVRuWlJQQ3QwSGZ6Yk1KR2YxU1E2WHhUZENta2tZeDVRZzZMbXpMUTF4dHlnQkI3Q2FXWjNNd2tab2ZmWDhYSkE9PTsyMDIwLTA4LTI2VDE0OjA3OjE1Ljg3MjU4MjVaO09lVllQYy8xOW5kdWVnTmhFMEJzMkE9PTt7Ik9yY2hlc3RyYXRpb25TdGVwIjoxfQ==&amp;tx=StateProperties=eyJUSUQiOiJjNTU3YTExOC03MTczLTRkZGQtOGQ5NS0xYTI3YWEzY2U2MjcifQ&amp;p=B2C_1_logintest"
-              >
-                Forgot your password?
-              </a>
+          <div className="entry">
+            <div className="entry-item">
+              <label htmlFor="email">Email Address</label>
+              <input
+                type="text"
+                id="email"
+                name="Email Address"
+                placeholder="Email Address"
+                value=""
+              />
             </div>
-            <input
-              type="password"
-              id="password"
-              name="Password"
-              placeholder="Password"
-            />
-          </div>
-          <div className="working"></div>
+            <div className="entry-item">
+              <div className="password-label">
+                <label htmlFor="password">Password</label>
+                <a
+                  id="forgotPassword"
+                  href="/futurenhsplatform.onmicrosoft.com/B2C_1_logintest/api/CombinedSigninAndSignup/forgotPassword?csrf_token=Q0tQNm51aUlXcXAzamVxNTFuL3c4aVRuWlJQQ3QwSGZ6Yk1KR2YxU1E2WHhUZENta2tZeDVRZzZMbXpMUTF4dHlnQkI3Q2FXWjNNd2tab2ZmWDhYSkE9PTsyMDIwLTA4LTI2VDE0OjA3OjE1Ljg3MjU4MjVaO09lVllQYy8xOW5kdWVnTmhFMEJzMkE9PTt7Ik9yY2hlc3RyYXRpb25TdGVwIjoxfQ==&amp;tx=StateProperties=eyJUSUQiOiJjNTU3YTExOC03MTczLTRkZGQtOGQ5NS0xYTI3YWEzY2U2MjcifQ&amp;p=B2C_1_logintest"
+                >
+                  Forgot your password?
+                </a>
+              </div>
+              <input
+                type="password"
+                id="password"
+                name="Password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="working"></div>
 
-          <div className="buttons">
-            <button id="next" type="submit" form="localAccountForm">
-              Sign in
-            </button>
+            <div className="buttons">
+              <button id="next" type="submit" form="localAccountForm">
+                Sign in
+              </button>
+            </div>
           </div>
-        </div>
-      </form> */}
-    </StyledDiv>
+        </form>
+      </StyledDiv>
+      <PasswordDiv>
+        <a
+          id="forgotPassword"
+          href="/futurenhsplatform.onmicrosoft.com/B2C_1_logintest/api/CombinedSigninAndSignup/forgotPassword?csrf_token=Q0tQNm51aUlXcXAzamVxNTFuL3c4aVRuWlJQQ3QwSGZ6Yk1KR2YxU1E2WHhUZENta2tZeDVRZzZMbXpMUTF4dHlnQkI3Q2FXWjNNd2tab2ZmWDhYSkE9PTsyMDIwLTA4LTI2VDE0OjA3OjE1Ljg3MjU4MjVaO09lVllQYy8xOW5kdWVnTmhFMEJzMkE9PTt7Ik9yY2hlc3RyYXRpb25TdGVwIjoxfQ==&amp;tx=StateProperties=eyJUSUQiOiJjNTU3YTExOC03MTczLTRkZGQtOGQ5NS0xYTI3YWEzY2U2MjcifQ&amp;p=B2C_1_logintest"
+        >
+          Forgot your password?
+        </a>
+      </PasswordDiv>
+    </LoginContainer>
   );
 };
 
